9e574e1dd047b2a071f1cbb773df93984dbec78c
Denis Novikov <denverden@mail.ru>
2020-08-21 22:36:33 +0300

    feat: added babel

--- webpack.config.js
+++ webpack.config.js
@@ -17,4 +17,20 @@ module.exports = {
       title: 'JS Course',
     }),
   ],
+  module: {
+    rules: [
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      }
+    ]
+  },
 };
