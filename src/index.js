2eae1f5552b09e66c994166e9e5b0cada20d4b4b
Denis Novikov <denverden@mail.ru>
2020-08-21 22:25:44 +0300

    feat: added solution es-features task 10

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ import { task6Old, task6New } from './es-features/task6';
 import { task7Old, task7New } from './es-features/task7';
 import { task8Old, task8New } from './es-features/task8';
 import { task9Old, task9New } from './es-features/task9';
-// import { task10Old, task10New } from './es-features/task10';
+import { task10Old, task10New } from './es-features/task10';
 // import { task11Old, task11New } from './es-features/task11';
 // import { task12Old, task12New } from './es-features/task12';
 // import { task13Old, task13New } from './es-features/task13';
@@ -78,10 +78,10 @@ console.log('task9 new', task9New()); // [true, 1]
 
 console.log('=============');
 
-// console.log('task10 old', task10Old()); // {name: 'Max', age: 12, color: red}
-// console.log('task10 new', task10New()); // {name: 'Max', age: 12, color: red}
+console.log('task10 old', task10Old()); // {name: 'Max', age: 12, color: red}
+console.log('task10 new', task10New()); // {name: 'Max', age: 12, color: red}
 
-// console.log('=============');
+console.log('=============');
 
 // console.log('task11 old', task11Old({a: null}, 'a')); // false
 // console.log('task11 new', task11New({a: null}, 'a')); // false
