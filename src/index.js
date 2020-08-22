797a7070eedd02ff3df235ee1dfe405af94b3545
Denis Novikov <denverden@mail.ru>
2020-08-23 00:39:10 +0300

    feat: added solution array-practice task 7

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ import { forEachRight } from './array-practice/task3';
 import { union } from './array-practice/task4';
 import { createGenerator } from './array-practice/task5';
 import { transformArrayToNumber } from './array-practice/task6';
+import { take } from './array-practice/task7';
 
 console.log('task1 old', task1Old(10) === 500); // true
 console.log('task1 new', task1New(10) === 500); // true
@@ -155,4 +156,9 @@ console.log('test 2 ( 70 ->', transformArrayToNumber([10, 20, 30], (acc, item) =
 console.log('test 3 ( 0 ->', transformArrayToNumber([10, 20, 30], (acc, item) => acc * item), ')');
 console.log('test 4 ( 6000 ->', transformArrayToNumber([10, 20, 30], (acc, item) => acc * item, 1), ')');
 console.log('test 5 ( -60 ->', transformArrayToNumber([10, 20, 30], (acc, item) => acc - item), ')');
+console.groupEnd();
+
+console.group('Task7');
+console.log('test 1 ( [1, 2, 3] ->', take([1, 2, 3], 5), ')');
+console.log('test 2 ( [1, 2] ->', take([1, 2, 3], 2), ')');
 console.groupEnd();
