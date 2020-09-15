9399ae0190faa69766ca83d3f110e017c285c024
Denis Novikov <denverden@mail.ru>
2020-09-15 09:40:28 +0300

    feat: added solution task 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './styles/main.css';
+import { foo, createCb } from './asynchronous-programming/task1';
 
-console.log('Hi');
+console.group('Task1');
+console.log(`test1 x=5`);
+foo(5, createCb('cb'));
+console.log(`test2 x=20`);
+foo(20, createCb('cb'));
+console.groupEnd();
