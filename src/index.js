dfff7107ebb0d56dfde7dd62a34f09285e950aa9
Denis Novikov <denverden@mail.ru>
2020-09-15 10:39:27 +0300

    feat: added solution task 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/main.css';
 import { foo, createCb } from './asynchronous-programming/task1';
 import { parseJSON, successCb, failureCb } from './asynchronous-programming/task2';
+import { delay } from './asynchronous-programming/task3';
 
 console.group('Task1');
 console.log(`test1 x=5`);
@@ -15,3 +16,9 @@ parseJSON('{"x": 10}', successCb, failureCb);
 console.log(`test2 {x}`);
 parseJSON('{x}', successCb, failureCb);
 console.groupEnd();
+
+(async () => {
+  console.group('Task3');
+  await delay(1000).then(value => console.log(`Done with ${value}`));
+  console.groupEnd();
+})();
